4e5f13352e4583629a0af209ab2559ddfc9c0a87
sherlyn99 <y1weng@ucsd.edu>
2024-10-01 10:31:10 -0700

    add _quant.py

---
+++ micov/_quant.py
@@ -0,0 +1,12 @@
+# sherlyn: from input to bin_df for a single genomem cli.py
+
+# caitlin: micov_calc, micov_main, _plots
+# - a polar dataframe of bin_df + other stuff
+# - pl.DataFrame(bin_df)
+
+# unit tests
+
+if __name__ == '__main__':
+    input1 = ""
+    input2 = ""
+    # run function
