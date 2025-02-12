cbe0de40d93c4f93a9cc999e5dc655f87a5c9e91
Daniel McDonald <damcdonald@ucsd.edu>
2025-02-12 10:13:25 -0700

    add duckdb to pip reqs

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 """micov: microbiome coverage."""
+
 import versioneer
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-
-setup(name='micov',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      license='BSD-3-Clause',
-      author='Daniel McDonald',
-      author_email='damcdonald@ucsd.edu',
-      packages=find_packages(),
-      install_requires=[
-          'polars-u64-idx >= 1.21',
-          'matplotlib >= 3.9',
-          'scipy',
-          'click',
-          'numba'],
-      entry_points='''
+setup(
+    name="micov",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license="BSD-3-Clause",
+    author="Daniel McDonald",
+    author_email="damcdonald@ucsd.edu",
+    packages=find_packages(),
+    install_requires=[
+        "polars-u64-idx >= 1.21",
+        "matplotlib >= 3.9",
+        "duckdb",
+        "scipy",
+        "click",
+        "numba",
+    ],
+    entry_points="""
           [console_scripts]
           micov=micov.cli:cli
-      ''')
+      """,
+)
